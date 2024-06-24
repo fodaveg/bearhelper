362cc69a7fbf2ef72f2a7f1e01c026d276934c3f
fodaveg <fodaveg@fodaveg.net>
2024-06-24 13:14:20 +0200

    Initial Commit

---
+++ BearHelper/BearHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearHelperApp.swift
+//  BearHelper
+//
+//  Created by David Velasco on 24/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BearHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
